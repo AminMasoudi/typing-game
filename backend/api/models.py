8e71aeedbbf38a7e1324049baef82e32e074409f
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-07-24 22:48:35 +0330

    models base

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User, models.DO_NOTHING, related_name="games")
+    start_time = models.TimeField(auto_created=True)
+    initial_score = models.IntegerField()
+    score = models.IntegerField(null=True)
+    end_time = models.TimeField()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    score = models.IntegerField(default=0)
+    
